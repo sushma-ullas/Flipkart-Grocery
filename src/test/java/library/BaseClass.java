cae08fe16550e46d04c7fecdd22cf967d135b185
sushma-ullas <sushmabhadresh1999@gmail.com>
2023-05-05 17:54:54 +0530

    Updated Base class with comments

--- src/test/java/library/BaseClass.java
+++ src/test/java/library/BaseClass.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+//Launch Browser and open flipkart website
 public class BaseClass {
 	public static WebDriver driver;
 	public static Logger logger = LogManager.getLogger(BaseClass.class.getName());
