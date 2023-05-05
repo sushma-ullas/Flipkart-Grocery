4662cd4d5e269d573b805136691c234a9cfd5b2e
sushma-ullas <sushmabhadresh1999@gmail.com>
2023-05-05 17:58:24 +0530

    Base Class Formatted

--- src/test/java/library/BaseClass.java
+++ src/test/java/library/BaseClass.java
@@ -14,33 +14,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 	public static WebDriver driver;
 	public static Logger logger = LogManager.getLogger(BaseClass.class.getName());
-	
-	public static void openBrowser(String browsername, String baseUrl){
-		if(browsername.equalsIgnoreCase("chrome"))
-		{
+
+	public static void openBrowser(String browsername, String baseUrl) {
+		if (browsername.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			
-			ChromeOptions options = new ChromeOptions();//base
+
+			ChromeOptions options = new ChromeOptions();// base
 			options.addArguments("--no-sandbox");
 			options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--remote-allow-origins=*");
 			driver = new ChromeDriver(options);
 			driver.get(baseUrl);
-		}else if(browsername.equalsIgnoreCase("firefox")){
+		} else if (browsername.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 			driver.get(baseUrl);
-			
-			
-		}else if(browsername.equalsIgnoreCase("edge")){
+
+		} else if (browsername.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 			driver.get(baseUrl);
-			
-			
+
 		}
-		}
-		
 	}
 
-
+}
