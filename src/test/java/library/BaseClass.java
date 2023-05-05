bcd1bb3ec22ae7b00a5922ca8b4b5480d39bf176
sushma-ullas <sushmabhadresh1999@gmail.com>
2023-05-05 17:48:54 +0530

    Initial setup - flipkart groceries automation

---
+++ src/test/java/library/BaseClass.java
@@ -0,0 +1,45 @@
+package library;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public static WebDriver driver;
+	public static Logger logger = LogManager.getLogger(BaseClass.class.getName());
+	
+	public static void openBrowser(String browsername, String baseUrl){
+		if(browsername.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			
+			ChromeOptions options = new ChromeOptions();//base
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--remote-allow-origins=*");
+			driver = new ChromeDriver(options);
+			driver.get(baseUrl);
+		}else if(browsername.equalsIgnoreCase("firefox")){
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.get(baseUrl);
+			
+			
+		}else if(browsername.equalsIgnoreCase("edge")){
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			driver.get(baseUrl);
+			
+			
+		}
+		}
+		
+	}
+
+
